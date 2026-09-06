55f5276f04addbd059f8fd82ef2f3bdb645a17e0
madebyRohitjha <rohit.jha.ic@gmail.com>
2026-09-06 17:04:52 +0900

    load and visualise the dataset

--- train.py
+++ train.py
@@ -1,3 +1,25 @@
 import tensorflow as tf
 
 print("TensorFlow Version:", tf.__version__)
+
+# Image size
+IMG_SIZE = (160, 160)
+
+# Batch size
+BATCH_SIZE = 32
+
+# Load training dataset
+train_dataset = tf.keras.utils.image_dataset_from_directory(
+    "dataset/train",
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE
+)
+
+# Load validation dataset
+validation_dataset = tf.keras.utils.image_dataset_from_directory(
+    "dataset/validation",
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE
+)
+
+print("Datasets loaded successfully!")
