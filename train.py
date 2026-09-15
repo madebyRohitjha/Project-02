001b1e7b6e8d2b4070ab7a294eb3af5b456209e9
madebyRohitjha <rohit.jha.ic@gmail.com>
2026-09-15 16:11:31 +0900

    Add second convolution and pooling layers

--- train.py
+++ train.py
@@ -72,3 +72,21 @@ for images, labels in train_dataset.take(1):
         input_shape=(160, 160, 3)
     )
 ])
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(
+        32,
+        (3, 3),
+        activation="relu",
+        input_shape=(160, 160, 3)
+    ),
+
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Conv2D(
+        64,
+        (3, 3),
+        activation="relu"
+    ),
+
+    tf.keras.layers.MaxPooling2D()
+])
