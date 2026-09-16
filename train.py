8d4a36187529b84fb995bcd414f39f70ea419ddd
madebyRohitjha <rohit.jha.ic@gmail.com>
2026-09-16 16:47:33 +0900

    Add Flatten layer to CNN

--- train.py
+++ train.py
@@ -89,4 +89,24 @@ model = tf.keras.Sequential([
     ),
 
     tf.keras.layers.MaxPooling2D()
+
+    model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(
+        32,
+        (3, 3),
+        activation="relu",
+        input_shape=(160, 160, 3)
+    ),
+
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Conv2D(
+        64,
+        (3, 3),
+        activation="relu"
+    ),
+
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Flatten()
 ])
