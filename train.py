ee0cbaa466c56765b4446c6db3ad3e3eb2d529aa
madebyRohitjha <rohit.jha.ic@gmail.com>
2026-09-17 14:37:23 +0900

    Add dense layer to CNN

--- train.py
+++ train.py
@@ -110,3 +110,26 @@ model = tf.keras.Sequential([
 
     tf.keras.layers.Flatten()
 ])
+tf.keras.layers.Dense(128, activation="relu")
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(
+        32,
+        (3, 3),
+        activation="relu",
+        input_shape=(160, 160, 3)
+    ),
+
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Conv2D(
+        64,
+        (3, 3),
+        activation="relu"
+    ),
+
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Flatten(),
+
+    tf.keras.layers.Dense(128, activation="relu")
+])
